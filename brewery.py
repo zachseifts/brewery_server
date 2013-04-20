23019a51c617a31ce03f0e2429c0ca0f844ffcf3
Zach Seifts <seiftsze@appstate.edu>
2013-04-19 22:51:47 -0400

    fixing conversion

--- brewery.py
+++ brewery.py
@@ -37,8 +37,8 @@ def after_request(response):
 
 @app.route('/', methods=['GET'])
 def home():
-    cur = g.db.execute('select created, temp, key from entries order by id desc')
-    entries = [dict(created=row[0], temp=float(row[1] / 1000), key=row[2]) for row in cur.fetchall()]
+    cur = g.db.execute('select created, temp/1000.0, key from entries order by id desc')
+    entries = [dict(created=row[0], temp=row[1], key=row[2]) for row in cur.fetchall()]
     return render_template('entries.html', entries=entries, graph=reversed(entries))
 
 @app.route('/temp/<key>/<int:temp>', methods=['POST'])
