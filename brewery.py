94c579f47be9b2e1bacee3289b85aa4767a0068e
Zach Seifts <zach.seifts@gmail.com>
2013-04-21 12:51:14 -0400

    moving over to mongodb

--- brewery.py
+++ brewery.py
@@ -1,6 +1,5 @@
 from __future__ import with_statement
 import os
-import sqlite3
 import datetime
 from contextlib import closing
 from flask import Flask, request, jsonify, json, Response, abort, g, render_template, send_from_directory
@@ -9,15 +8,14 @@ from flask.ext.mongoengine import MongoEngine
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-Bootstrap(app)
-app.config.from_envvar('FLASK_SETTINGS', silent=True)
 
+app.config.from_envvar('FLASK_SETTINGS', silent=True)
 app.config['BOOTSTRAP_USE_MINIFIED'] = True
 app.config['BOOTSTRAP_USE_CDN'] = True
 app.config['BOOTSTRAP_FONTAWESOME'] = True
-
 app.config['MONGODB_SETTINGS'] = {'DB': 'brewery_server'}
 
+Bootstrap(app)
 db = MongoEngine()
 db.init_app(app)
 
@@ -28,39 +26,40 @@ class Reading(db.Document):
     key = db.StringField(max_length=60)
     value = db.IntField()
 
+    def as_celsus(self):
+        ''' Returns the temperature as celsus.
+        '''
+        return self.value / 1000.0
 
-def connect_db():
-    ''' Connects to the database.
-    '''
-    return sqlite3.connect(app.config['DATABASE'])
+    def as_fahrenheit(self):
+        ''' Returns the temperature as fahrenheit.
+        '''
+        return (self.as_celsus() * 1.8) + 32
 
-def init_db():
-    ''' Creates the database from sql.
-    '''
-    with closing(connect_db()) as db:
-        with app.open_resource('database.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
 
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.after_request
-def after_request(response):
-    g.db.close()
-    return response
+@app.template_filter('reverse')
+def reverse_filter(s):
+    return reversed(s)
 
 @app.route('/', methods=['GET'])
 def home():
-    cur = g.db.execute('select created, ((temp/1000.0) * 1.8) + 32, temp/1000.0, key from entries order by id desc limit 1440')
-    entries = [dict(created=row[0], temp=row[1], c=row[2], key=row[3]) for row in cur.fetchall()]
+    readings = list()
+    for reading in list(Reading.objects.order_by('created')[:1440]):
+        readings.append({
+            'created': reading.created,
+            'temp': reading.as_fahrenheit(),
+            'c': reading.as_celsus(),
+            'key': reading.key
+        })
+    avg_temp = Reading.objects.order_by('created')[:1440].average('value');
     return render_template('homepage.html',
-        entries=entries[:60],
-        current=entries[0]['temp'], 
-        half=reversed(entries[:30]),
-        hour=reversed(entries[:60]),
-        day=reversed(entries))
+        average=(((avg_temp / 1000.0) * 1.8) + 32),
+        high=Reading.objects.order_by('-value')[0].as_fahrenheit(),
+        low=Reading.objects.order_by('value')[0].as_fahrenheit(),
+        current=readings[0]['temp'], 
+        hour=readings[:60],
+        half=readings[:30],
+        day=readings)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -73,9 +72,7 @@ def temps(key, temp):
         auth = request.authorization
         if not auth or (auth.username != app.config['DEFAULT_USERNAME'] or auth.password != app.config['DEFAULT_PASSWORD']):
             abort(401)
-        g.db.execute('insert into entries (temp, key) values (?, ?)', [temp, key])
-        g.db.commit()
-        r = Reading(key=key, value=temp).save()
+        Reading(key=key, value=temp).save()
         return jsonify(status='ok', temp=temp, key=key)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
