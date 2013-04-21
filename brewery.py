c3f385d0f9d9688be76b5ef1d8cdd60c687c8676
Zach Seifts <zach.seifts@gmail.com>
2013-04-21 13:13:49 -0400

    moing more stuff

--- brewery.py
+++ brewery.py
@@ -44,7 +44,7 @@ def reverse_filter(s):
 @app.route('/', methods=['GET'])
 def home():
     readings = list()
-    objects = Reading.objects.order_by('created')[:1440]
+    objects = Reading.objects.order_by('created')
     for reading in list(objects):
         readings.append({
             'created': reading.created,
