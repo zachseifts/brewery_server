abc3100aef754ef1c252b3aa33bddd1c5f21bf4a
Zach Seifts <zach.seifts@gmail.com>
2013-04-21 17:30:42 -0400

    updating fab

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,9 @@
 from __future__ import with_statement
-from fabric.api import run, cd, sudo
+from fabric.api import run, cd, sudo, local
 
 def update_app():
+    with cd('/Users/zach/projects/brewery_server'):
+        local('git push')
     with cd('/home/zach/apps/brewery_server'):
         run('git pull')
         sudo('supervisorctl restart brewery_server')
